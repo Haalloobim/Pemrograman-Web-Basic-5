3ecad8a54db74c8b2166aa5b22dafa1306232804
Haalloobim <withbimatara@gmail.com>
2023-10-24 16:51:56 +0700

    feat: add variabel to script.js

--- src/script.js
+++ src/script.js
@@ -3,7 +3,11 @@ const { createApp, ref } = Vue
 createApp({
     data() {
         return {
-            message: "hello world!",
+            message: "Vue Implementation - Calculator",
+            angka1: 0, 
+            angka2: 0, 
+            operasi: 0,
+            res: 0, 
         }
     },
     
