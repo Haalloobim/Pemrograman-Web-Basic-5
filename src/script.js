711b987c23e17d0bf536e4944b54a2ca8cebd088
Haalloobim <withbimatara@gmail.com>
2023-10-24 17:00:40 +0700

    feat: add js calculator methods

--- src/script.js
+++ src/script.js
@@ -10,5 +10,18 @@ createApp({
             res: 0, 
         }
     },
-    
+    methods: {
+        plus(){
+            this.res = this.angka1 + this.angka2
+        }, 
+        min(){
+            this.res = this.angka1 - this.angka2
+        },
+        kali(){
+            this.result = this.angka1 * this.angka2
+        }, 
+        bagi(){
+            this.res = this.angka1 / this.angka2
+        },
+    }
 }).mount('#app')
