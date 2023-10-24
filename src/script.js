654786c89b9a5dc060b8e2ab418bc9da3fdf33db
Haalloobim <withbimatara@gmail.com>
2023-10-24 17:15:21 +0700

    feat: showing list using vue js

--- src/script.js
+++ src/script.js
@@ -3,11 +3,42 @@ const { createApp, ref } = Vue
 createApp({
     data() {
         return {
-            message: "Vue Implementation - Calculator",
+            message: "Vue Implementation - Calculator & Task Tagged",
             angka1: 0, 
             angka2: 0, 
-            operasi: 0,
             res: 0, 
+            List: [
+                {
+                    nama: "Bima",
+                    task: "ETS P-WEB", 
+                    isTrue: true
+                }, 
+                {
+                    nama: "Jeri",
+                    task: "ETS Teori Graf", 
+                    isTrue: true
+                }, 
+                {
+                    nama: "Iftala",
+                    task: "Web Development", 
+                    isTrue: true
+                }, 
+                {
+                    nama: "Aditya",
+                    task: "ETS Matdis", 
+                    isTrue: true
+                }, 
+                {
+                    nama: "Kaisar",
+                    task: "Codeforces Thing", 
+                    isTrue: true
+                }, 
+                {
+                    nama: "Moris", 
+                    task: "ETS PBO", 
+                    isTrue: true
+                }
+            ],
         }
     },
     methods: {
@@ -18,7 +49,7 @@ createApp({
             this.res = this.angka1 - this.angka2
         },
         kali(){
-            this.result = this.angka1 * this.angka2
+            this.res = this.angka1 * this.angka2
         }, 
         bagi(){
             this.res = this.angka1 / this.angka2
